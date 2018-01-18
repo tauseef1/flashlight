1b6cb310197a042564e5f13ccdd08104bec6f877
Tauseef Malik <tauseef1@gmail.com>
2018-01-18 12:34:56 -0500

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isOn : boolean;
+  constructor(private flashLight: Flashlight, private platform: Platform) {    
+    this.platform.ready().then(()=>{
+      this.updateFlashlightStatus();
+    });
+  }
 
-  constructor(public navCtrl: NavController) {
+  switchOn(){
+    this.flashLight.switchOn();
+    this.updateFlashlightStatus();
+  }
 
+  switchOff(){
+    this.flashLight.switchOff();
+    this.updateFlashlightStatus();
+  }
+
+  toggle(){
+    this.flashLight.toggle();
+    this.updateFlashlightStatus();
+  }
+
+  private updateFlashlightStatus(){
+    this.isOn = this.flashLight.isSwitchedOn();
   }
 
 }
